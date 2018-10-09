42f9b9b27e3815f3ce866b2bfb5feb42cc46548a
eastskim <eastskim48@gmail.com>
2018-10-10 03:58:43 +0900

    크롤러

--- crawling/views.py
+++ crawling/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from selenium import webdriver
+import time
 # Create your views here.
 
-def index(request):
-    return HttpResponse("hi")
+def tagnum(request,pk):
+    driver = webdriver.Chrome('chrome/chromedriver')
+    driver.get('https://www.instagram.com/tags/'+pk)
+    time.sleep(1) #안전성을 위해서 넣어야됨
+    rst = driver.find_elements_by_class_name('g47SY')[0]
+    driver.quit()
+    return HttpResponse(rst.text)
+
+def pictures(requests):
+    return HttpResponse("준비중입니다")
--- pospot/urls.py
+++ pospot/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include,url
+from pospot import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^crawling/', include('crawling.urls')),
+    url(r'^crawling/tagnum/(?P<pk>["[^ ㄱ-ㅣ가-힣]+"]+)$', views.tagnum, name='tagnum'),
+    url(r'^crawling/pictures', views.tagnum,name='pictures'),
 ]
