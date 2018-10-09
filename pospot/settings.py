f8c746b1451f6a5aa2b1ee3f0830ea1df7f6c583
Ubuntu <pospot@PospotAPI.5etuw3erd1eexlgf25f5jpt5ag.ix.internal.cloudapp.net>
2018-10-09 18:28:30 +0000

    init set

--- pospot/settings.py
+++ pospot/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '!%8_qr=$*p88_^xcw+4_^$^^l@odlo*pjofjb505mwsae&ae)7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -118,3 +118,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
