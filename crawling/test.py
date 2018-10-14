eab625deb6c60d304ba8f0ecad2d3eaea4dcf533
Ubuntu <pospot@PospotAPI.5etuw3erd1eexlgf25f5jpt5ag.ix.internal.cloudapp.net>
2018-10-14 10:53:15 +0000

    test crawler 성공

--- crawling/test.py
+++ crawling/test.py
@@ -3,11 +3,15 @@ from django.http import HttpResponse
 from selenium import webdriver
 import time
 import os
+from selenium.webdriver.chrome.options import Options
 # Create your views here.
-driver = webdriver.Chrome(os.getcwd())
-driver.set_page_load_timeout(my_timeout_secs)
+options=Options()
+options.add_argument('--headless')
+driver = webdriver.Chrome('/usr/bin/chromedriver',chrome_options=options)
+driver.set_page_load_timeout(10)
 driver.get('https://www.instagram.com/tags/paris')
 time.sleep(1) #안전성을 위해서 넣어야됨
 rst = driver.find_elements_by_class_name('g47SY')[0]
+print(rst.text.replace(',', ''))
 driver.quit()
 
